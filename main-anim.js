dc3650b5e7413ab553330df4eda68b27475bb8f7
oskarbednarek <ob317537@student.polsl.pl>
2025-02-06 21:26:27 +0100

    some fixes. TODO : improve animations

--- main-anim.js
+++ main-anim.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll("nav ul li a");
     navLinks.forEach(link => {
         link.addEventListener("click", function(event) {
-            event.preventDefault();
             const target = event.target.getAttribute("href");
-            document.querySelector(target).scrollIntoView({
-                behavior: "smooth"
-            });
+            const targetElement = document.querySelector(target);
+            if (targetElement) {
+                event.preventDefault();
+                targetElement.scrollIntoView({
+                    behavior: "smooth"
+                });
+                // Update the URL hash without jumping to the element
+                history.pushState(null, null, target);
+            }
         });
     });
 });

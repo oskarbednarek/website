e671a809459cac37372c6ab1630e64b1f181d457
Oskar Bednarek <ob317537@student.polsl.pl>
2025-04-07 22:08:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,1 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
-});
